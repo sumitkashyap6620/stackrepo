767982b1946c4cc2a091101de66818bdb3f97a4d
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-01-02 01:03:28 +0530

    class for Statcks created

---
+++ linkedimplementation.java
@@ -0,0 +1,22 @@
+package stackrepo;
+import  java.util.Scanner;
+
+ class node {
+    int value ;
+    node next ;
+ }
+ class Stack{
+node head = new node ();
+int counter = 0 ;
+void push (int x ){
+
+}
+ }
+public class linkedimplementation {
+    public static void main(String []args){
+     Scanner sc = new Scanner(System.in);
+        Stack s1 = new Stack ();
+     System.out.println("To push press 1 : \nTo pull press 2 : \nTo print stack press 3 : \nTo get size of your Stack press 4 : \nTo get the peek value press 5 : ");
+           
+    }
+}
