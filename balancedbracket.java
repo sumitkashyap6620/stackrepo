eea9282e0b2b60b8c8135edf77f28f9873b13971
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-01-03 10:58:54 +0530

    remove logical error

--- balancedbracket.java
+++ balancedbracket.java
@@ -6,16 +6,21 @@ public class balancedbracket {
     static void CheckBracket(String B ){
     Stack<Character> s = new Stack<>();
     int l = B.length();
+     
     for(int i = 0 ; i < l ; i++ ){
-        if(s.isEmpty() == true && B.charAt(i) == ')')s.push(')');
-       else {
-        if (B.charAt(i)=='(')s.push('(');
+        if(s.isEmpty() == true && B.charAt(i) == ')'){
+            s.push(')');
+            
+      }
+        else {
+        if (B.charAt(i)==')' && s.peek() == '(' )s.pop();
         else 
-            s.pop();
+            s.push(B.charAt(i));
        }
     }  
     if (s.size() == 0)System.out.println("Brackets are Balanced :-");
      else System.out.println("Brackets are not Balanced :-");
+     System.out.println("brackets to remove become a balance bracket : "+s.size());
     }
     public static void main(String[] args) {
     System.out.print("give any series of'(' ')' brackets to check balanced bracket :-\n");
